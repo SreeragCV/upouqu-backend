d9c548eaaece4805892ad1b6231fc25b45038c13
Sreerag Mohan <sreeraagmohancv@gmail.com>
2024-02-23 14:43:30 +0530

    genre routes and controllers created

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const {
   userProfile,
   getAllUsers,
 } = require("./controller/user");
-const { contributeBook, getHorrorBooks } = require("./controller/book");
+const { contributeBook, getHorrorBooks, getNovelBooks, getPsychologyBooks, getShortStoryBooks, getPhilosophyBooks, getLiteratureBooks, getHistoryBooks, getRomanceBooks, getMysteryBooks, getFictionBooks, getPoetryBooks, getBiographyBooks, getActionBooks, getScienceFictionBooks, getFantasyBooks, getHumorBooks, getThrillerBooks } = require("./controller/book");
 const { verifyToken, isSuperAdmin } = require("./middleware/authorization");
 const multer = require("multer");
 const PORT = process.env.PORT || 8080;
@@ -41,7 +41,23 @@ app.get("/is-auth", verifyToken, isAuth);
 app.get("/contribute", verifyToken, contributeBook);
 app.get("/user/:id", verifyToken, userProfile);
 app.post("/contribute", multiUpload, verifyToken, contributeBook);
-app.get("/horror-books", getHorrorBooks);
+app.get("/psychology", getPsychologyBooks);
+app.get("/thriller", getThrillerBooks);
+app.get("/novel", getNovelBooks);
+app.get("/short-story", getShortStoryBooks);
+app.get("/philosophy", getPhilosophyBooks);
+app.get("/literature", getLiteratureBooks);
+app.get("/history", getHistoryBooks);
+app.get("/romance", getRomanceBooks);
+app.get("/mystery", getMysteryBooks);
+app.get("/fiction", getFictionBooks);
+app.get("/poetry", getPoetryBooks);
+app.get("/biography", getBiographyBooks);
+app.get("/action", getActionBooks);
+app.get("/horror", getHorrorBooks);
+app.get("/science-fiction", getScienceFictionBooks);
+app.get("/fantsay", getFantasyBooks);
+app.get("/humor", getHumorBooks);
 app.get("/all-users", verifyToken, isSuperAdmin, getAllUsers);
 
 app.listen(PORT, () => {
--- controller/book.js
+++ controller/book.js
@@ -94,38 +94,216 @@ module.exports.contributeBook = async function (req, res) {
   }
 };
 
+// Horror
 exports.getHorrorBooks = async (req, res) => {
   try {
     const horrorBooks = await pool.query(
       `SELECT * FROM Books WHERE 'Horror' = ANY (genre);`
     );
     return res.status(200).json({ books: horrorBooks, message: "SUCCESS!" });
-  } catch (e) {}
+  } catch (e) {
+    return res.status(500).json({ message: "Server Error" });
+  }
 };
 
+// Action
 exports.getActionBooks = async (req, res) => {
   try {
     const actionBooks = await pool.query(
       `SELECT * FROM Books WHERE 'Action' = ANY (genre);`
     );
-    return res.status(200).json({ books: horrorBooks, message: "SUCCESS!" });
-  } catch (e) {}
+    return res.status(200).json({ books: actionBooks, message: "SUCCESS!" });
+  } catch (e) {
+    return res.status(500).json({ message: "Server Error" });
+  }
 };
 
+// Thriller
 exports.getThrillerBooks = async (req, res) => {
   try {
     const thrillerBooks = await pool.query(
       `SELECT * FROM Books WHERE 'Thriller' = ANY (genre);`
     );
-    return res.status(200).json({ books: horrorBooks, message: "SUCCESS!" });
-  } catch (e) {}
+    return res.status(200).json({ books: thrillerBooks, message: "SUCCESS!" });
+  } catch (e) {
+    return res.status(500).json({ message: "Server Error" });
+  }
 };
 
+// Romance
 exports.getRomanceBooks = async (req, res) => {
   try {
     const romanceBooks = await pool.query(
       `SELECT * FROM Books WHERE 'Romance' = ANY (genre);`
     );
-    return res.status(200).json({ books: horrorBooks, message: "SUCCESS!" });
-  } catch (e) {}
+    return res.status(200).json({ books: romanceBooks, message: "SUCCESS!" });
+  } catch (e) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// Psychology
+exports.getPsychologyBooks = async (req, res) => {
+  try {
+    const psychologyBooks = await pool.query(
+      `SELECT * FROM Books WHERE 'Psychology' = ANY (genre);`
+    );
+    return res
+      .status(200)
+      .json({ books: psychologyBooks, message: "SUCCESS!" });
+  } catch (e) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// Novel
+exports.getNovelBooks = async (req, res) => {
+  try {
+    const novelBooks = await pool.query(
+      `SELECT * FROM Books WHERE 'Novel' = ANY (genre);`
+    );
+    return res.status(200).json({ books: novelBooks, message: "SUCCESS!" });
+  } catch (e) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// Short Story
+exports.getShortStoryBooks = async (req, res) => {
+  try {
+    const shortStoryBooks = await pool.query(
+      `SELECT * FROM Books WHERE 'Short Story' = ANY (genre);`
+    );
+    return res
+      .status(200)
+      .json({ books: shortStoryBooks, message: "SUCCESS!" });
+  } catch (e) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// Literature
+exports.getLiteratureBooks = async (req, res) => {
+  try {
+    const literatureBooks = await pool.query(
+      `SELECT * FROM Books WHERE 'Literature' = ANY (genre);`
+    );
+    return res
+      .status(200)
+      .json({ books: literatureBooks, message: "SUCCESS!" });
+  } catch (e) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// History
+exports.getHistoryBooks = async (req, res) => {
+  try {
+    const historyBooks = await pool.query(
+      `SELECT * FROM Books WHERE 'History' = ANY (genre);`
+    );
+    return res.status(200).json({ books: historyBooks, message: "SUCCESS!" });
+  } catch (e) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// Fiction
+exports.getFictionBooks = async (req, res) => {
+  try {
+    const fictionBooks = await pool.query(
+      `SELECT * FROM Books WHERE 'Fiction' = ANY (genre);`
+    );
+    return res.status(200).json({ books: fictionBooks, message: "SUCCESS!" });
+  } catch (e) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// Poetry
+exports.getPoetryBooks = async (req, res) => {
+  try {
+    const poetryBooks = await pool.query(
+      `SELECT * FROM Books WHERE 'Poetry' = ANY (genre);`
+    );
+    return res.status(200).json({ books: poetryBooks, message: "SUCCESS!" });
+  } catch (e) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// Biography
+exports.getBiographyBooks = async (req, res) => {
+  try {
+    const biographyBooks = await pool.query(
+      `SELECT * FROM Books WHERE 'Biography' = ANY (genre);`
+    );
+    return res.status(200).json({ books: biographyBooks, message: "SUCCESS!" });
+  } catch (e) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// Fantsay
+exports.getFantasyBooks = async (req, res) => {
+  try {
+    const fantasyBooks = await pool.query(
+      `SELECT * FROM Books WHERE 'Fantasy' = ANY (genre);`
+    );
+    return res.status(200).json({ books: fictionBooks, message: "SUCCESS!" });
+  } catch (e) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// Mystery
+exports.getMysteryBooks = async (req, res) => {
+  try {
+    const mysteryBooks = await pool.query(
+      `SELECT * FROM Books WHERE 'Mystery' = ANY (genre);`
+    );
+    return res.status(200).json({ books: mysteryBooks, message: "SUCCESS!" });
+  } catch (e) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// Science-Fiction
+exports.getScienceFictionBooks = async (req, res) => {
+  try {
+    const scienceFictionBooks = await pool.query(
+      `SELECT * FROM Books WHERE 'Science Fiction' = ANY (genre);`
+    );
+    return res
+      .status(200)
+      .json({ books: scienceFictionBooks, message: "SUCCESS!" });
+  } catch (e) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// Philosophy
+exports.getPhilosophyBooks = async (req, res) => {
+  try {
+    const philosophyBooks = await pool.query(
+      `SELECT * FROM Books WHERE 'Philosophy' = ANY (genre);`
+    );
+    return res
+      .status(200)
+      .json({ books: philosophyBooks, message: "SUCCESS!" });
+  } catch (e) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// Science-Fiction
+exports.getHumorBooks = async (req, res) => {
+  try {
+    const humorBooks = await pool.query(
+      `SELECT * FROM Books WHERE ' Humor' = ANY (genre);`
+    );
+    return res.status(200).json({ books: humorBooks, message: "SUCCESS!" });
+  } catch (e) {
+    return res.status(500).json({ message: "Server Error" });
+  }
 };
